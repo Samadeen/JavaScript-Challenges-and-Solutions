11906b394fbd2055cf9641e64fcabc968aceddb8
Momoh Abdulsamad <samadmomoh@gmail.com>
2022-09-05 08:56:50 +0100

    Simple Array Sum HackerRank

---
+++ Simple-Array-Sum.js
@@ -0,0 +1,10 @@
+// Given an array of integers, find the sum of its elements.
+
+// For example, if the array ar = [1,2,3], 1 + 2 + 3 so return 6.
+
+const simpleArrSum = (ar) => {
+  let sum = 0;
+  for (let i = 0; i < ar.length; i++) {
+    sum += ar[i];
+  }
+};
