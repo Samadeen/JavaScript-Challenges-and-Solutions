5efac791d0db3a982b27fcbc1da3ae3e953bf91a
Momoh Abdulsamad <samadmomoh@gmail.com>
2023-01-29 20:12:37 +0100

    Update Valid Parentheses.js

--- Valid Parentheses.js
+++ Valid Parentheses.js
@@ -29,3 +29,24 @@ function validParentheses(parens) {
 
 //   return stack.length === 0;
 // }
+
+// var isValid = function (s) {
+//   // Initialize stack to store the closing brackets expected...
+//   let stack = [];
+//   // Traverse each character in input string...
+//   for (let idx = 0; idx < s.length; idx++) {
+//     // If open parentheses are present, push it to stack...
+//     if (s[idx] == '{') {
+//       stack.push('}');
+//     } else if (s[idx] == '[') {
+//       stack.push(']');
+//     } else if (s[idx] == '(') {
+//       stack.push(')');
+//     }
+//     // If a close bracket is found, check that it matches the last stored open bracket
+//     else if (stack.pop() !== s[idx]) {
+//       return false;
+//     }
+//   }
+//   return !stack.length;
+// };
